13a75ce0f15d8bb399d018d98a2561b867f7bb07
antonio1092 <antonio.cioffi92@yahoo.it>
2019-06-22 15:46:37 +0200

    sesto commit

--- SpringEcommerce/src/main/java/com/ecommerce/webapp/SpringEcommerceApplication.java
+++ SpringEcommerce/src/main/java/com/ecommerce/webapp/SpringEcommerceApplication.java
@@ -2,6 +2,7 @@ package com.ecommerce.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SpringEcommerceApplication {
--- SpringEcommerce/src/main/webapp/jsp/index.jsp
+++ SpringEcommerce/src/main/webapp/jsp/index.jsp
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Benvenuto</title>
 <p>BENVENUTO</p>
-<a href="inserimentoarticolo.jsp">Inserisci</a>
+<a href="jsp/inserimentoarticolo.jsp">Inserisci</a>
 </head>
 <body>
 </body>
